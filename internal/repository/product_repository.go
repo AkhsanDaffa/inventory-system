a5f7c6581a7df8212a8dfb8b1a1f149a1e626aa6
ADAP2001 <akhsandaffa01@gmail.com>
2025-12-08 17:04:07 +0700

    Add category into products table

--- internal/repository/product_repository.go
+++ internal/repository/product_repository.go
@@ -8,10 +8,13 @@ import (
 )
 
 type Product struct {
-	ID       string `json:"id"`
-	Name     string `json:"name" validate:"required"`
-	SKU      string `json:"sku" validate:"required"`
-	Quantity int    `json:"quantity" validate:"gte=0"`
+	ID         string `json:"id"`
+	Name       string `json:"name" validate:"required"`
+	SKU        string `json:"sku" validate:"required"`
+	Quantity   int    `json:"quantity" validate:"gte=0"`
+	CategoryID string `json:"category_id"`
+
+	CategoryName string `json:"category_name,omitempty"`
 }
 
 type ProductRepository struct {
@@ -20,12 +23,12 @@ type ProductRepository struct {
 
 func (r *ProductRepository) CreateProduct(ctx context.Context, p *Product) error {
 	query := `
-		INSERT INTO products (name, sku, quantity)
-		VALUES ($1, $2, $3)
+		INSERT INTO products (name, sku, quantity, category_id)
+		VALUES ($1, $2, $3, $4)
 		RETURNING id
 	`
 
-	err := r.DB.QueryRow(ctx, query, p.Name, p.SKU, p.Quantity).Scan(&p.ID)
+	err := r.DB.QueryRow(ctx, query, p.Name, p.SKU, p.Quantity, p.CategoryID).Scan(&p.ID)
 
 	if err != nil {
 		return fmt.Errorf("failed Insert Database: %w", err)
@@ -37,7 +40,15 @@ func (r *ProductRepository) CreateProduct(ctx context.Context, p *Product) error
 func (r *ProductRepository) GetAllProducts(ctx context.Context) ([]Product, error) {
 	products := []Product{}
 
-	query := "SELECT id, name, sku, quantity FROM products"
+	// query := "SELECT id, name, sku, quantity FROM products"
+	query := `
+	SELECT
+		p.id, p.name, p.sku, p.quantity, p.category_id,
+		COALESCE(c.name, '') as category_name
+	FROM products p
+	LEFT JOIN categories c ON p.category_id = c.id
+	`
+
 	rows, err := r.DB.Query(ctx, query)
 	if err != nil {
 		return nil, fmt.Errorf("failed Query: %w", err)
@@ -46,8 +57,16 @@ func (r *ProductRepository) GetAllProducts(ctx context.Context) ([]Product, erro
 
 	for rows.Next() {
 		var p Product
-		if err := rows.Scan(&p.ID, &p.Name, &p.SKU, &p.Quantity); err != nil {
+		// if err := rows.Scan(&p.ID, &p.Name, &p.SKU, &p.Quantity); err != nil {
+		// 	return nil, fmt.Errorf("failed to scan: %w", err)
+		// }
+		var catID *string
+		if err := rows.Scan(&p.ID, &p.Name, &p.SKU, &p.Quantity, &catID, &p.CategoryName); err != nil {
 			return nil, fmt.Errorf("failed to scan: %w", err)
+		}
+
+		if catID != nil {
+			p.CategoryID = *catID
 		}
 		products = append(products, p)
 	}
